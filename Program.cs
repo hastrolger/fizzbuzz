7d71fda534ca74ef269a8b2307c45ace220707c1
hastrolger <parralesjael@gmail.com>
2023-04-20 14:14:11 -0700

    fizzbuzz application completed

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿void fizzbuzz () {
+    for (var n = 1; n < 101; n++){
+        if (n % 3 == 0 && n % 5 == 0) 
+        {
+            Console.WriteLine($"Number {n} is a FIZZBUZZ");
+        }
+        else if (n % 3 == 0) 
+        {
+            Console.WriteLine($"Number {n} is a FIZZ");
+        }
+        else if (n % 5 == 0) 
+        {
+            Console.WriteLine($"Number {n} is a BUZZ");
+        } 
+        else 
+        {
+            Console.WriteLine($"Number {n} is not FIZZ nor BUZZ");
+        }
+        
+    }
+}
+
+fizzbuzz();
